6b13814cc2b84bf82679f679408abe6bca861e3d
Wooksang Ryu <today8934@gmail.com>
2022-04-03 17:01:40 +0900

    SecurityContextHolder & Authentication

--- src/main/java/me/today8934/demospringsecurityform/form/SampleController.java
+++ src/main/java/me/today8934/demospringsecurityform/form/SampleController.java
@@ -9,6 +9,12 @@ import java.security.Principal;
 @Controller
 public class SampleController {
 
+    private final SampleService sampleService;
+
+    public SampleController(SampleService sampleService) {
+        this.sampleService = sampleService;
+    }
+
     @GetMapping("/")
     public String index(Model model, Principal principal) {
         if (principal == null) {
@@ -28,6 +34,7 @@ public class SampleController {
 
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal) {
+        sampleService.dashboard();
         model.addAttribute("message", "Hello" + principal.getName());
         return "dashboard";
     }
